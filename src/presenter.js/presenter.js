f7f22b8dc8350cf20228a1d7c4e73a62c44d8357
nathan95oks <nathanaelalba75@gmail.com>
2025-04-24 03:23:10 -0400

    Implementacion del formulario y validacion de campos

--- src/presenter.js/presenter.js
+++ src/presenter.js/presenter.js
@@ -1,4 +1,21 @@
 import './iniciarSesion'
-import  './registroUsuario'
+import { registroUsuario } from './registroUsuario.js';
 import './solicitarTicketCarga'
 import './reportarIngresoGasolina'
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const form = document.getElementById('registro-form');
+    const resultadoDiv = document.getElementById('resultado-div');
+
+    form.addEventListener('submit' , (e) => {
+        e.preventDefault();
+        const nombre = document.getElementById('nombre').value;
+        const correo = document.getEelementById('correo').value;
+        const contrasena = document.getElementById('constrasena').value;
+        const tipoUsuario = document.getElementById('tipo-usuario').value;
+
+        const resultado = registroUsuario(nombre,correo,contrasena,tipoUsuario);
+        resultadoDiv.textContent = resultado;
+    })
+})
