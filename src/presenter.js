42f6d327a77687090b03b46bee7bddee61536025
nathan95oks <nathanaelalba75@gmail.com>
2025-05-05 21:39:36 -0400

    agregar  surtidor con su tipo de gasolina - Front

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,12 @@
-// src/presenter.js
 import { reportarIngresoGasolina } from './reportarIngresoGasolina.js';
+import { agregarSurtidor } from './agregarSurtidor.js';
 
 const form = document.getElementById('Ingreso-form');
 const reporteDiv = document.getElementById('reporte-gasolina');
 const limpiarBtn = document.getElementById('limpiar-datos-btn');
+const surtidorForm = document.getElementById('surtidor-form');
+const reporteSurtidores = document.getElementById('reporte-surtidores');
+const surtidores = [];
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -25,6 +28,26 @@ form.addEventListener('submit', (e) => {
   form.reset();
 });
 
-limpiarBtn.addEventListener('click', () => {
-  reporteDiv.innerHTML = '';
+
+surtidorForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nombre = document.getElementById('nombre-surtidor').value.trim();
+  const tipo = document.getElementById('tipo-surtidor').value.trim();
+
+  const resultado = agregarSurtidor(nombre, tipo);
+  if (resultado.success) {
+    surtidores.push(resultado.data);
+    actualizarVistaSurtidores();
+    surtidorForm.reset();
+  }
+
+  alert(resultado.message);
 });
+
+
+function actualizarVistaSurtidores() {
+  reporteSurtidores.innerHTML = '<h3>Lista de Surtidores:</h3>' + surtidores.map(s =>
+    `<p><strong>${s.nombre}</strong> - ${s.tipoCombustible}</p>`
+  ).join('');
+}
+
